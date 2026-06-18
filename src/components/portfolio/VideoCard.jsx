cbaf9cd717c5f2340f247d375a513c53eb3135e4
Mukku766 <jagdeeprrajput@gmail.com>
2026-06-18 06:37:09 +0530

    [Update]:Code pushed with refraction

--- src/components/portfolio/VideoCard.jsx
+++ src/components/portfolio/VideoCard.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef} from "react";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import Icon from "../common/Icon";
